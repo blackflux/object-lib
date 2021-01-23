67c1a4d41bfc6478be91721fed4bd9bf77e9fcd0
Lukas Siemon <github@blackflux.com>
2021-01-22 23:00:30 -0800

    feat: added merge

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,8 @@ describe('Testing index.js', () => {
   it('Testing exported', () => {
     expect(Object.keys(index)).to.deep.equal([
       'align',
-      'contain'
+      'contain',
+      'Merge'
     ]);
   });
 });
