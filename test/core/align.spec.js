681fb53ddd2883e47247e56c7bbf582d8faf09a0
Lukas Siemon <github@blackflux.com>
2021-01-26 20:17:37 -0800

    fix: align now using object-scan

--- test/core/align.spec.js
+++ test/core/align.spec.js
@@ -1,12 +1,28 @@
 const expect = require('chai').expect;
+const cloneDeep = require('lodash.clonedeep');
+const { describe } = require('node-tdd');
 const align = require('../../src/core/align');
+const genData = require('./gen-data');
 
 describe('Testing align', () => {
+  const convert = (input) => JSON.stringify(input, null, 2).split('\n');
   const runTest = (input, template, expected) => {
     align(input, template);
-    expect(JSON.stringify(input, null, 2).split('\n'))
+    expect(convert(input))
       .to.deep.equal(JSON.stringify(expected, null, 2).split('\n'));
   };
+
+  it('Batch test', ({ fixture }) => {
+    const alignRec = fixture('align-rec');
+    for (let x = 0; x < 10000; x += 1) {
+      const tree1 = genData();
+      const tree2 = cloneDeep(tree1);
+      const ref = genData();
+      align(tree1, ref);
+      alignRec(tree2, ref);
+      expect(convert(tree1)).to.deep.equal(convert(tree2));
+    }
+  });
 
   it('Testing Basic Ordering', () => {
     const obj = { key1: 'value1', key2: 'value2' };
