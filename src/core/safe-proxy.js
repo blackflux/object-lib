500f50e01090ff0612e27c3fcf4566091f2affab
Lukas Siemon <lukas.siemon@blackflux.com>
2025-03-31 11:13:44 -0700

    feat: added SafeProxy

---
+++ src/core/safe-proxy.js
@@ -0,0 +1,47 @@
+const BYPASS_PROPS = [
+  Symbol.toStringTag,
+  Symbol.toPrimitive,
+  Symbol.iterator,
+  Symbol.hasInstance,
+  'inspect',
+  'toString',
+  'valueOf',
+  'constructor',
+  'then',
+  'length'
+];
+
+// Proxy that errors when accessing non-existent properties
+const SafeProxy = (tgt, path = '') => {
+  // eslint-disable-next-line @blackflux/rules/prevent-typeof-object
+  if (tgt === null || typeof tgt !== 'object') {
+    return tgt;
+  }
+
+  return new Proxy(tgt, {
+    get(target, prop, receiver) {
+      if (BYPASS_PROPS.includes(prop) && !Object.prototype.hasOwnProperty.call(target, prop)) {
+        return Reflect.get(target, prop, receiver);
+      }
+
+      const propStr = typeof prop === 'symbol' ? prop.toString() : String(prop);
+
+      if (!(prop in target)) {
+        const currentPath = path ? `${path}.${propStr}` : propStr;
+        throw new Error(`Property '${currentPath}' does not exist`);
+      }
+
+      const value = Reflect.get(target, prop, receiver);
+
+      // eslint-disable-next-line @blackflux/rules/prevent-typeof-object
+      if (value !== null && typeof value === 'object') {
+        const currentPath = path ? `${path}.${propStr}` : propStr;
+        return SafeProxy(value, currentPath);
+      }
+
+      return value;
+    }
+  });
+};
+
+export default SafeProxy;
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as clone } from './core/clone.js';
 export { default as contains } from './core/contains.js';
 export { default as Merge } from './core/merge.js';
 export { default as jsonSmartParse } from './core/json-smart-parse.js';
+export { default as SafeProxy } from './core/safe-proxy.js';
